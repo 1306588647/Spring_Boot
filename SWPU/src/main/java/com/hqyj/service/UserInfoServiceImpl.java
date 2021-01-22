f7368c500c63126d2a43bfcb324a920662ad3c97
SpongeBob <49099073+1306588647@users.noreply.github.com>
2021-01-22 13:19:45 +0800

    update

--- SWPU/src/main/java/com/hqyj/service/UserInfoServiceImpl.java
+++ SWPU/src/main/java/com/hqyj/service/UserInfoServiceImpl.java
@@ -14,7 +14,6 @@ import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
-import java.awt.print.Pageable;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Random;
